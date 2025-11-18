6d7c4e4be8dbbccae9fc73a87c8b223aadf0dfdc
axel8verde <112900700+axel8verde@users.noreply.github.com>
2025-11-18 19:42:42 +0100

    Committing 4 items from workspace e86c0172-5cc1-4296-9188-0a8fc683996a

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "b33b4eb3-98ab-45ed-8c99-627357f79c2f",
+# META       "default_lakehouse_name": "Bronce_Landing",
+# META       "default_lakehouse_workspace_id": "e86c0172-5cc1-4296-9188-0a8fc683996a",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "b33b4eb3-98ab-45ed-8c99-627357f79c2f"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
